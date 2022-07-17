55a73866440061eea9737df052e8aa3dab822632
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-17 10:24:44 -0400

    Update quiz.py

--- big-quiz/quiz.py
+++ big-quiz/quiz.py
@@ -1,2 +1,16 @@
 WIDTH = 1280
 HEIGHT = 720
+main_box = Rect(0, 820, 240)
+timer_box1 = Rect(0, 0, 240, 240)
+answer_box1 = Rect(0, 0, 495, 165)
+answer_box2 = Rect(0, 0, 495, 165)
+answer_box3 = Rect(0, 0, 495, 165)
+answer_box4 = Rect(0,0, 495, 165)
+def draw():
+  pass
+def game_over():
+  pass
+def on_mouse_down():
+  pass
+def update_time_left():
+  pass
